651e09e6a9aaf73ffb8b4c082f38633acbb02d62
koppuhemanthsaikumar <koppuhemanthsaikumar123@gmail.com>
2026-02-11 23:39:25 +0530

    Initial stage of chat app

--- index.js
+++ index.js
@@ -1,22 +1,62 @@
-const express = require('express');
-const http = require('http');
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
 const app = express();
 const server = http.createServer(app);
-const path = require('path');
-const { Server } = require('socket.io');
 const io = new Server(server);
 
-io.on('connection', (socket) => {
-  console.log('a user connected', socket.id);  
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.use(express.static(path.join(__dirname, 'public')));
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
 
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
 });
 
 server.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  console.log("Server running on port 3000");
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const express = require('express');
+// const http = require('http');
+// const app = express();
+// const server = http.createServer(app);
+// const path = require('path');
+// const { Server } = require('socket.io');
+// const io = new Server(server);
+
+// io.on('connection', (socket) => {
+//   console.log('a user connected', socket.id);  
+// });
+
+// app.use(express.static(path.join(__dirname, 'public')));
+
+
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// });
+
+// server.listen(3000, () => {
+//   console.log('Server is running on port 3000');
+// });
